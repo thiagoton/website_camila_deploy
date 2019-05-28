c819cac959f4853a90e23d2f0d83151913474246
Thiago <thiago@pumatronix.com.br>
2019-05-28 17:52:18 -0300

    [MASTER] Reducing size of logo. Minnor fixes

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@
 
 	var fullHeight = function() {
 
-		if ( !isMobile.any() ) {
+		if ( true ) {
 			$('.js-fullheight').css('height', $(window).height());
 			$(window).resize(function(){
 				$('.js-fullheight').css('height', $(window).height());
